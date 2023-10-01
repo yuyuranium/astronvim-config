af463cecb2a4afb927473b7f23c58486ad7af20b
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-10-01 18:57:12 +0800

    Add support for BSV and restructure configs

--- plugins/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    signs = {
-      delete = { text = "_" },
-      topdelete = { text = "‾" },
-    },
-  },
-}
--- plugins/user.lua
+++ plugins/user.lua
@@ -10,9 +10,44 @@ return {
   --   end,
   -- },
   {
-    "https://github.com/LunarVim/darkplus.nvim.git"
+    "LunarVim/darkplus.nvim"
   },
   {
     "max397574/better-escape.nvim", enabled = false
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      local ft = require "Comment.ft"
+      ft.bsv = {'//%s', '/*%s*/'}
+    end
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    opts = {
+      at_edge = 'stop',
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+      },
+    },
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      override = {
+        bsv = {
+          icon = "󰘚",
+          color = "#51a0cf",
+          cterm_color = "74",
+          name = "BSV",
+        }
+      },
+    }
   }
 }
