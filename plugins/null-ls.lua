4800d4898be8762e442a2adda37f6a4587afd0ad
yuyuranium <n28111534@gs.ncku.edu.tw>
2023-12-22 11:33:32 +0800

    Add bsv for null-ls and disable format on save

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,6 +1,80 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
+    local null_ls = require "null-ls"
+    local h = require("null-ls.helpers")
+    local methods = require("null-ls.methods")
+    local bsv_lint = h.make_builtin({
+      name = "bsc",
+      meta = {
+        url = "https://github.com/B-Lang-org/bsc",
+        description = "Bluespec Compiler",
+      },
+      method = methods.internal.DIAGNOSTICS,
+      filetypes = { "bsv" },
+      generator_opts = {
+        command = "bsc",
+        args = { "-sim", "-elab", "-bdir", "/tmp", "-p", ".:%/Libraries:$DIRNAME", "-u", "$FILENAME" },
+        to_stdin = false,
+        from_stderr = true,
+        to_temp_file = true,
+        format = "raw",
+        on_output = function(params, done)
+          local output = params.output
+          if not output then
+            return done()
+          end
+
+          local severity = {
+            Error = 1,
+            Warning = 2
+          }
+
+          local diagnostics = {}  -- all diagnostics
+          local diagnostic = {}   -- single diagnostic
+
+          local lines = vim.split(output, "\n")
+          for _, line in ipairs(lines) do
+            if line ~= "" then
+              local err, filename, row, col, code =
+              line:match("(.*): \"(.*)\", line (%d*), column (%d*): %((.*)%)")
+              if row ~= nil and col ~= nil then
+                -- parse succeed, line is header
+                if next(diagnostic) ~= nil then
+                  -- if previous diagnostic exists, insert into diagnostics
+                  table.insert(diagnostics, diagnostic)
+                end
+
+                diagnostic = {
+                  filename = filename,
+                  row = row,
+                  col = col,
+                  code = code,
+                  source = "bsv",
+                  message = "",
+                  severity = severity[err],
+                }
+              else
+                -- parse failed, line is message
+                if next(diagnostic) ~= nil then
+                  -- make sure diagnostics table is not empty
+                  diagnostic.message = diagnostic.message .. string.sub(line, 3) .. "\n"
+                end
+              end
+            end
+          end
+
+          if next(diagnostic) ~= nil then
+            -- if previous diagnostic exists, insert into diagnostics
+            table.insert(diagnostics, diagnostic)
+          end
+
+          done(diagnostics)
+        end,
+      },
+      factory = h.generator_factory,
+    })
+    null_ls.register(bsv_lint)
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
 
