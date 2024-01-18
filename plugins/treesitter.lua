ab0eb588f8578355d2a18f72778ae1a3f44874c6
yuyuranium <n28111534@gs.ncku.edu.tw>
2024-01-18 14:36:07 +0800

    Don't use treesitter indent for c and cpp

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -16,8 +16,10 @@ return {
 
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua", "c", "cpp"
     })
+
+    opts.indent = { disable = { "c", "cpp" } }
   end,
   dependencies = {
     {
